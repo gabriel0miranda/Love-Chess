a864f9541fc215a29e6d4b4f2be4bf23d405a1c5
Gabriel Miranda <gabriel.miranda@poli.digital>
2024-07-08 17:11:59 -0300

    WIP: pieces.

--- piece_list.lua
+++ piece_list.lua
@@ -11,6 +11,25 @@ local Component = require('common-components')
 return {
     new_pawn = function()
         local pawn = Component.new_piece(1)
-    end
+    end,
 
+    new_knight = function()
+        local pawn = Component.new_piece(2)
+    end,
+
+    new_bishop = function()
+        local pawn = Component.new_piece(3)
+    end,
+
+    new_rook = function()
+        local pawn = Component.new_piece(4)
+    end,
+
+    new_queen = function()
+        local pawn = Component.new_piece(5)
+    end,
+
+    new_king = function()
+        local pawn = Component.new_piece(6)
+    end,
 }
